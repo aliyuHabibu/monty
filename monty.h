dbeabff30bed70e18c608c736ce000dcfc18820e
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-13 20:26:24 +0100

    Add header file

---
+++ monty.h
@@ -0,0 +1,69 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFSIZE 1
+#define BUFFER 1024
+
+extern int args;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instructions_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function that takes in a pointer to a character, and return a pointer to
+ * a funcction that takes a stack and line number and returns void
+ */
+void (*get_opcode_func(char *s))(stack_t **stack, unsigned int line_number);
+
+/* Miccelenaneous Functions handling test chases */
+void read_and_exec_file(char *file);
+int str_len(char *s);
+void clear_array(char *str);
+void set_args(char *str);
+/*bool str_cmp(char *s1, char *s2);*/
+
+/* Opcode Functons */
+void push_op(stack_t **stack, unsigned int line_number);
+void pall_op(stack_t **stack, unsigned int line_number);
+void pint_op(stack_t **stack, unsigned int line_number);
+void pop_op(stack_t **stack, unsigned int line_number);
+void swap_op(stack_t **stack, unsigned int line_number);
+void add_op(stack_t **stack, unsigned int line_number);
+void sub_op(stack_t **stack, unsigned int line_number);
+void div_op(stack_t **stack, unsigned int line_number);
+void mul_op(stack_t **stack, unsigned int line_number);
+void mod_op(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
