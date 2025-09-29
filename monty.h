55d4c93d7e90ad745fd870f331f431027bc88a33
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-29 23:01:36 +0100

    Update header file

--- monty.h
+++ monty.h
@@ -42,20 +42,28 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Function that takes in a pointer to a character, and return a pointer to
+/**
+ * Function that takes in a pointer to a character, and return a pointer to
  * a funcction that takes a stack and line number and returns void
  */
-void (*get_opcode_func(char *s))(stack_t **stack, unsigned int line_number);
+void (*sk(char *s, unsigned int l))(stack_t **stack, unsigned int line_number);
+void (*qu(char *s, unsigned int l))(stack_t **stack, unsigned int line_number);
 
 /* Miccelenaneous Functions handling test chases */
 void read_and_exec_file(char *file);
 int str_len(char *s);
 void clear_array(char *str);
-void set_args(char *str);
+void set_args(char *str, unsigned int line_number);
+void handle_comment(char *buffer, char *array, int *n);
+void handle_indicator(char *str, int *indicator);
+void check_file_name(char *file_name, int fd);
+void handle_two_node(stack_t **stack);
+void check_exec_op(int id, char *s, stack_t *h, unsigned int line);
 /*bool str_cmp(char *s1, char *s2);*/
 
 /* Opcode Functons */
-void push_op(stack_t **stack, unsigned int line_number);
+void push_op_sk(stack_t **stack, unsigned int line_number);
+void push_op_qu(stack_t **stack, unsigned int line_number);
 void pall_op(stack_t **stack, unsigned int line_number);
 void pint_op(stack_t **stack, unsigned int line_number);
 void pop_op(stack_t **stack, unsigned int line_number);
@@ -65,5 +73,8 @@ void sub_op(stack_t **stack, unsigned int line_number);
 void div_op(stack_t **stack, unsigned int line_number);
 void mul_op(stack_t **stack, unsigned int line_number);
 void mod_op(stack_t **stack, unsigned int line_number);
+void pchar_op(stack_t **stack, unsigned int line_number);
+void pstr_op(stack_t **stack, unsigned int line_number);
+void rotl_op(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
