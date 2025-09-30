67a5277591ff16ffb32dfe0ae24177ca56eef6b0
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-09-30 21:25:37 +0100

    Implement the nop opcode.:
    	The nop opcode
    
    	The opcode nop doesn’t do anything.
    
    	Usage: nop

---
+++ nop_op.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop_op - Function that deos nothig
+ * @stack: double pointer to the head of the stack
+ * @line_number: line number command found
+ *
+ * Return: nothing.
+ */
+void nop_op(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
